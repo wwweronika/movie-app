8452a0d8ce44c9b39c008ef7e6e2928790278861
MG174 <goryczko99@gmail.com>
2019-01-19 22:25:30 +0100

    Updates

---
+++ precache-manifest.13a66775159bdd5e23de8afd13b01c48.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e976f319d0b6e61242e5",
+    "url": "/movie-app/static/js/main.e976f319.chunk.js"
+  },
+  {
+    "revision": "4e9f71ad0b3e6cee88bc",
+    "url": "/movie-app/static/js/1.4e9f71ad.chunk.js"
+  },
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "6ff1834587beae130a02529d6c1d39e6",
+    "url": "/movie-app/index.html"
+  }
+];
