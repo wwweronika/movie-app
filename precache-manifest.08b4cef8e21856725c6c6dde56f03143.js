a50eeb6a7f9c2f6c150d8d07989ea09183e7c5fa
MG174 <goryczko99@gmail.com>
2019-01-20 12:37:05 +0100

    Updates

---
+++ precache-manifest.08b4cef8e21856725c6c6dde56f03143.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f78091a12019d0c7692",
+    "url": "/movie-app/static/js/main.8f78091a.chunk.js"
+  },
+  {
+    "revision": "099de8660194ff544c51",
+    "url": "/movie-app/static/js/1.099de866.chunk.js"
+  },
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "b45182383cef54681b752ca35b1d4452",
+    "url": "/movie-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie-app/precache-manifest.24b617d4578cb16792fd49614f827398.js"
+  "/movie-app/precache-manifest.08b4cef8e21856725c6c6dde56f03143.js"
 );
 
 workbox.clientsClaim();
